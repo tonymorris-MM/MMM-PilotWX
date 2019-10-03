1bbed4b8a944279d6f2c48d02626151b1010de86
Adam Morris <amorris2003@gmail.com>
2019-10-04 08:45:29 +1000

    Use metar.online

--- node_helper.js
+++ node_helper.js
@@ -1,10 +1,3 @@
-/* Magic Mirror
- *
- * Module: MMM-PilotWX
- *
- * By Mykle1
- * 
- */
 const NodeHelper = require('node_helper');
 const request = require('request');
 const parser = require('xml2js').parseString;
@@ -16,27 +9,9 @@ module.exports = NodeHelper.create({
     	console.log("Starting module: " + this.name);
     },
     
-    getWISP: function(url) {
-    	request({ 
-    	          url: url,
-    	          method: 'GET' 
-    	        }, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                parser(body, (err, result)=> {
-						var result = JSON.parse(JSON.stringify(result.response.data));
-					//	console.log(result); // for checking
-					//	console.log(response.statusCode); // for checking
-                        this.sendSocketNotification("WISP_RESULT", result);
-                   
-                });
-            }
-       });
-    },
-
-    //Subclass socketNotificationReceived received.
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_WISP') {
-                this.getWISP(payload);
-            }
-         }  
-    });
+    getMetar: function(site) {
+        request(site, function(error, response, body) {
+            return body;
+        });
+    }
+});
